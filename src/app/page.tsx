0c59069126822584910ba6e9a3a970b4f366db6e
Junior Green <juniorgreen9185@hotmail.com>
2024-02-15 19:48:00 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,9 +19,9 @@ export default function Home() {
   const iconSize = 20
   const phoneNumber = "6475718450"
   const email = "juniorgreen9185@hotmail.com"
-  const title = "Full Stack Engineer"
+  const title = "Full Stack Developer"
   const name = "Junior Green"
-  const desc = "Full Stack Engineer proficient in crafting robust, modern software solutions."
+  const desc = "Full Stack Developer proficient in crafting robust, modern software solutions."
 
   function scrollToRef(ref: RefObject<HTMLDivElement>) {
     ref.current?.scrollIntoView({ behavior: 'smooth' });
